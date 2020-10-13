ea70c2cde98317b1d1aa46edbae00d51730ab4ab
Sergei Zlygostev <seregazlodei@gmail.com>
2020-10-13 20:21:50 +0300

    Revert "Bootstrap-vue is here" (before bootstrap)
    This reverts commit 8fbcf95b50aeb595716765e5382dedc4d1536648.

--- src/main.js
+++ src/main.js
@@ -2,12 +2,6 @@ import Vue from 'vue'
 import store from './store'
 import App from './App.vue'
 import router from './router'
-import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
-
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
-
-Vue.use(BootstrapVue)
 
 Vue.config.productionTip = false
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <h2>Home page</h2>
-        <b-link href="/blog">Blog</b-link>
+        <a href="/blog">Blog</a>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores quibusdam aperiam reiciendis? Cumque maiores laudantium, iure deleniti temporibus sit esse. Et vitae, dolorem eveniet optio inventore voluptas suscipit a aperiam!</p>
 
     </div>
